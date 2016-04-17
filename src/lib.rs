b3d60848a7acaa8f66b253efff296fcb631ab470
Robert Lord <robert@lord.io>
2016-04-17 16:54:18 -0500

    Add back in state transition error covariance matrix

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ struct KalmanFilter {
     state: DVector<f64>,
     cov: DMatrix<f64>, // variance-covariance matrix
     a: DMatrix<f64>, // transforms old states into new states
-    a_err: DVector<f64>, // transforms old states into new states
+    a_err: DMatrix<f64>, // error covariance matrix
     b: DMatrix<f64>, // transforms controls into new state deltas
     c: DMatrix<f64>, // transforms a state into sensor data
 }
@@ -25,7 +25,7 @@ impl KalmanFilter {
             state: DVector::from_slice(state_count, state),
             cov: DMatrix::from_col_vector(state_count, state_count, cov),
             a: DMatrix::from_col_vector(state_count, state_count, a),
-            a_err: DVector::from_slice(state_count, a_err),
+            a_err: DMatrix::from_col_vector(state_count, state_count, a_err),
             b: DMatrix::from_col_vector(control_count, state_count, b),
             c: DMatrix::from_col_vector(state_count, sensor_count, c),
         }
@@ -35,7 +35,7 @@ impl KalmanFilter {
         let sensor_data = DVector::from_slice(sensor_data_raw.len(), sensor_data_raw);
 
         let state_bar = &self.a * &self.state + &self.b * &control_data;
-        let cov_bar = &self.a * &self.cov * &self.a.transpose()/*TODO + self.a_err*/;
+        let cov_bar = &self.a * &self.cov * &self.a.transpose() + &self.a_err;
         let kalman_gain = &cov_bar * &self.c.transpose() * (&self.c * &cov_bar * &self.c.transpose()).inverse().unwrap();
         self.state = state_bar.clone() + &kalman_gain * (sensor_data - &self.c * state_bar.clone());
         self.cov = (new_identity::<DMatrix<f64>>(state_bar.len()) - &kalman_gain * &self.a) * cov_bar;
@@ -47,7 +47,7 @@ mod test {
     use KalmanFilter;
     #[test]
     fn it_works() {
-        KalmanFilter::new(&vec![0.0,0.0], &vec![0.0,0.0,0.0,0.0], &vec![0.0,0.0,0.0,0.0], &vec![0.0,0.0], &vec![0.0,0.0,0.0,0.0], &vec![0.0,0.0,0.0,0.0]);
+        KalmanFilter::new(&vec![0.0,0.0], &vec![0.0,0.0,0.0,0.0], &vec![0.0,0.0,0.0,0.0], &vec![0.0,0.0,0.0,0.0], &vec![0.0,0.0,0.0,0.0], &vec![0.0,0.0,0.0,0.0]);
         assert!(true);
     }
 }
